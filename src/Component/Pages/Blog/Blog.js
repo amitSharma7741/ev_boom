585b3d0d413c7113de3c7a0e431898ffc828984f
Amit <183099@nith.ac.in>
2022-12-11 15:43:05 +0530

    add side bar image

--- src/Component/Pages/Blog/Blog.js
+++ src/Component/Pages/Blog/Blog.js
@@ -23,8 +23,38 @@ function Blog() {
                 <div className="col-12 col-md-8 col-lg-8">
                     <Outlet />
                 </div>
-                <div className="col-12 col-md-4 col-lg-4">
-                    <div className="card" style={{ width: "18rem" }}>
+                <div className="col-12 col-md-4 col-lg-4 d-flex justify-content-center" style={{
+                   
+                }}>
+                    <div className="card w-100  text-white" style={{
+                        height:"300px",
+                        maxWidth:"350px",
+                        borderRadius:"20px",
+
+                    }}>
+                        <img src="https://static.autox.com/uploads/2021/11/Ola-Electric-S1-Pro.jpg" className="card-img h-100" alt="ola s1 pro"  style={{
+                            borderRadius:"20px",
+                        }}/>
+                        <div className="card-img-overlay d-flex justify-content-center">
+                            <h5 className="card-title" style={{
+                                fontWeight:"800",
+                                fontSize:"20px",
+                                color:"white",
+                                marginTop:"230px",
+                                backgroundColor:"black",
+                                padding:"0 10px",
+                                borderRadius:"10px"
+
+                            }}>
+                                Ola S1 Pro
+                            </h5>
+                            {/* <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text">Last updated 3 mins ago</p> */}
+                        </div>
+                    </div>
+
+
+                 {/*    <div className="card" style={{ width: "18rem" }}>
                         <img src="https://static.autox.com/uploads/2021/11/Ola-Electric-S1-Pro.jpg" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
@@ -33,7 +63,7 @@ function Blog() {
 
                         </div>
 
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
