7e55e6a4e1405537fb6d3a5294629216ea62cd58
Amit <183099@nith.ac.in>
2022-12-25 00:46:45 +0530

    add comparsion

--- src/Component/Pages/Scooter/Post/Post.js
+++ src/Component/Pages/Scooter/Post/Post.js
@@ -1,43 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import "./Post.css"
+import React, { useEffect, useState } from "react";
+import "./Post.css";
 import { useParams } from "react-router-dom";
 // how to import json file
-import scooter from "../Databse/ScooterData.json"
-import FourSpecification from './Component/FourSpecification'
-import EmiCalaculator from './Component/EmiCalaculator'
-import SimilarScooter from './Component/SimilarScooter'
-import ImageCarousel from './Component/ImageCarousel'
+import scooter from "../Databse/ScooterData.json";
+import FourSpecification from "./Component/FourSpecification";
+import EmiCalaculator from "./Component/EmiCalaculator";
+import SimilarScooter from "./Component/SimilarScooter";
+import ImageCarousel from "./Component/ImageCarousel";
 // import blob from "./Component/SVG/scooter.svg"
-import RunningCostCalculator from './Component/RunningCostCalculator';
-import ImageGallery from './Component/ImageGallery';
-import ReactGA from 'react-ga';
+import RunningCostCalculator from "./Component/RunningCostCalculator";
+import ImageGallery from "./Component/ImageGallery";
+import ReactGA from "react-ga";
 // import TryEmiCalculator from './Component/TryEmiCalculator';
-import { useMediaQuery } from 'react-responsive'
+import { useMediaQuery } from "react-responsive";
+import Camparsion from "../Component/Camparsion";
 const Post = () => {
-
     const { post } = useParams();
 
-    const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
-
+    const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
 
     const showData = scooter.filter((item) => item.path === post);
     // const realData = scooter.find((item) => item.id === 1)
     // console.log(realData)
 
-    // add read more button in info className tag 
-    const title = showData[0].scootername + " Price - Range, Images, Colours"
+    // add read more button in info className tag
+    const title = showData[0].scootername + " Price - Range, Images, Colours";
 
-    const [isreadMore, setIsreadMore] = useState(true)
+    const [isreadMore, setIsreadMore] = useState(true);
     const text = showData[0].longDescription;
 
     const ForMobile = () => {
         return (
             <>
-                <div className="fixed-bottom" >
-                    <div className="row bg-dark" style={{
-                        backgroundColor: "rgba(0,0,0,0.5)",
-                        height: "50px",
-                    }}>
+                <div className="fixed-bottom">
+                    <div
+                        className="row bg-dark"
+                        style={{
+                            backgroundColor: "rgba(0,0,0,0.5)",
+                            height: "50px",
+                        }}
+                    >
                         <div className="col-6 d-flex justify-content-center">
                             <button className="btn  text-white btn-block">
                                 {showData[0].scootername}
@@ -45,28 +47,27 @@ const Post = () => {
                         </div>
                         <div className="col-6  d-flex justify-content-center">
                             <button className="btn  text-white btn-block">
-                                Rs.  {showData[0].price}
+                                Rs. {showData[0].price}
                             </button>
                         </div>
                     </div>
-
                 </div>
             </>
-        )
-    }
+        );
+    };
 
     useEffect(() => {
         ReactGA.pageview(window.location.pathname + window.location.search);
         // open always in top
-        window.scrollTo(0, 0)
-    }, [])
+        window.scrollTo(0, 0);
+    }, []);
 
     const styles = {
         detailText: {
             width: isMobile ? "100%" : "75%",
             textAlign: "justify",
-        }
-    }
+        },
+    };
     return (
         <>
             {/*  <header>
@@ -89,32 +90,28 @@ const Post = () => {
             </div>
             </header> */}
             <div className="header">
-                <div className=" position-absolute " style={{
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    color: "white",
-                    textAlign: "center",
-                    fontSize: "30px",
-                    fontWeight: "bold",
-                    textShadow: "2px 2px 4px #000000",
-                    backgroundColor: "rgba(0, 0, 0, 0.5)",
-                    padding: "20px",
-                    borderRadius: "10px",
-                    // backgroundImage: `url(${blob})`,
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center"
-
-
-
-                }}>
-                    <p className='text-center'  >
-                        {showData[0].scootername}
-                    </p>
-                    <p className='text-center'  >
-                        {showData[0].price}
-                    </p>
+                <div
+                    className=" position-absolute "
+                    style={{
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        color: "white",
+                        textAlign: "center",
+                        fontSize: "30px",
+                        fontWeight: "bold",
+                        textShadow: "2px 2px 4px #000000",
+                        backgroundColor: "rgba(0, 0, 0, 0.5)",
+                        padding: "20px",
+                        borderRadius: "10px",
+                        // backgroundImage: `url(${blob})`,
+                        backgroundSize: "cover",
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "center",
+                    }}
+                >
+                    <p className="text-center">{showData[0].scootername}</p>
+                    <p className="text-center">{showData[0].price}</p>
                     {/* <div className='card   text-white' style={{
                         backgroundColor: "rgba(0, 0, 0, 0)",
                         border: "0px solid",
@@ -140,7 +137,10 @@ const Post = () => {
                 />
             </div>
 
-            <section className="section pt-0 position-relative pull-top" style={{ marginTop: "-100px" }}>
+            <section
+                className="section pt-0 position-relative pull-top"
+                style={{ marginTop: "-100px" }}
+            >
                 <div className="container">
                     <div className="rounded shadow dataContainer bg-white">
                         {/*  <div className="row">
@@ -208,34 +208,35 @@ const Post = () => {
                                             <p>Awards</p>
                                         </div>
                                     </div> */}
-
                         </div>
                         {/* short info */}
-                        <div className='mt-5'>
+                       {/*  <div className="mt-5">
                             <h1>Info</h1>
-                            <p className='text-black' style={styles.detailText}>
+                            <p className="text-black" style={styles.detailText}>
                                 {text.slice(0, 200)}
                             </p>
                             {/*    <button onClick={() => setIsreadMore(!isreadMore)}>{isreadMore ? "Read More" : "Read Less"}</button> */}
-                        </div>
+                        {/* </div> */} 
 
                         {/* emi calculator */}
                         <div className="mt-5">
                             <EmiCalaculator price={showData[0].price} />
                         </div>
 
-
-
                         {/*  specification */}
-                        <div className='row ' style={{
-                            marginTop: "100px"
-                        }}>
+                        <div
+                            className="row "
+                            style={{
+                                marginTop: "100px",
+                            }}
+                        >
                             <div className="col-lg-6 col-md-6 col-12">
                                 <h1>Specification</h1>
-                                <table className="table table-striped" style={{ maxWidth: "500px" }}>
-                                    <thead>
-                                        Pro Highlights
-                                    </thead>
+                                <table
+                                    className="table table-striped"
+                                    style={{ maxWidth: "500px" }}
+                                >
+                                    <thead>Pro Highlights</thead>
                                     <tbody>
                                         <tr>
                                             <td>Top Speed</td>
@@ -262,58 +263,56 @@ const Post = () => {
                             </div>
                             <div className="col-lg-6 col-md-6 col-12">
                                 {/* image carousel */}
-                                <ImageCarousel galleryData={showData[0].gallery} altText={title} />
+                                <ImageCarousel
+                                    galleryData={showData[0].gallery}
+                                    altText={title}
+                                />
                             </div>
-
                         </div>
-
-
-
 
                         {/* full detail */}
 
-                        <div className='' style={{
-                            marginTop: "100px"
-                        }}>
+                        <div
+                            className=""
+                            style={{
+                                marginTop: "100px",
+                            }}
+                        >
                             <h1>Full Detail</h1>
-                            <p className='text-black' style={styles.detailText}>
+                            <p className="text-black" style={styles.detailText}>
                                 {isreadMore ? text.slice(0, 300) : text}
                             </p>
-                            <button onClick={() => setIsreadMore(!isreadMore)}>{isreadMore ? "Read More" : "Read Less"}</button>
+                            <button onClick={() => setIsreadMore(!isreadMore)}>
+                                {isreadMore ? "Read More" : "Read Less"}
+                            </button>
                         </div>
-
 
                         {/* Running Cost Calculator */}
 
-                        <div style={{
-                            marginTop: "100px"
-                        }}>
+                        <div
+                            style={{
+                                marginTop: "100px",
+                            }}
+                        >
                             <RunningCostCalculator />
                         </div>
+                        {/* comparision */}
+                        <Camparsion/>
 
                         {/* similar scooter */}
                         <SimilarScooter name="ola-s1-pro" />
-
-
-
-
 
                         <ImageGallery galleryData={showData[0].gallery} altText={title} />
 
                         {/* try new emi calculator */}
                         {/* <TryEmiCalculator price={showData[0].price}  /> */}
-
-
                     </div>
                 </div>
             </section>
 
             {isMobile ? <ForMobile /> : ""}
-
         </>
-    )
-}
+    );
+};
 
-
-
-export default Post
+export default Post;
