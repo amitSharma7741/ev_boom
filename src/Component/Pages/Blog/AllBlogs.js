4cfdfd7867bcc8606fc90c42db217aa68d891026
Amit <183099@nith.ac.in>
2022-12-25 00:42:18 +0530

    add react markdown

--- src/Component/Pages/Blog/AllBlogs.js
+++ src/Component/Pages/Blog/AllBlogs.js
@@ -1,33 +1,45 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const AllBlogs = () => {
-    const navigate = useNavigate();
-    return (
-        <>
-            <div className="container">
-                <div className="d-flex justify-content-center">
-
-
-                    <div className="card mb-3" style={{ maxWidth: 540 }}
-                        onClick={() => navigate("/blog/1")}
-                        >
-                        <div className="row g-0">
-                            <div className="col-md-4">
-                                <img src=" https://static.autox.com/uploads/2021/11/Ola-Electric-S1-Pro.jpg" className="img-fluid rounded-start h-100" alt="..." />
-                            </div>
-                            <div className="col-md-8">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text text-black">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="container">
+        <div className="d-flex justify-content-center">
+          <div
+            className="card mb-3"
+            style={{ maxWidth: 540 }}
+            onClick={() => navigate("/blog/1")}
+          >
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img
+                  src=" https://static.autox.com/uploads/2021/11/Ola-Electric-S1-Pro.jpg"
+                  className="img-fluid rounded-start h-100"
+                  alt="..."
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text text-black">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
                 </div>
-                
-                <div className="d-flex justify-content-center">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="d-flex justify-content-center">
 
 
                     <div className="card mb-3" style={{ maxWidth: 540 }}>
@@ -44,9 +56,9 @@ const AllBlogs = () => {
                             </div>
                         </div>
                     </div>
-                </div>
-                
-              {/*   <div className="d-flex justify-content-center">
+                </div> */}
+
+        {/*   <div className="d-flex justify-content-center">
 
 
                     <div className="card mb-3" style={{ maxWidth: 540 }}>
@@ -64,12 +76,10 @@ const AllBlogs = () => {
                         </div>
                     </div>
                 </div> */}
-                
-
-            </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 // suggest a image link
 
-export default AllBlogs
+export default AllBlogs;
