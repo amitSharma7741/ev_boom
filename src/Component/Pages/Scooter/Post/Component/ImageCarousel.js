b46e5da5404c977bc6358c9e174377df2dcef95e
Amit <183099@nith.ac.in>
2022-12-28 01:06:56 +0530

    pretify code

--- src/Component/Pages/Scooter/Post/Component/ImageCarousel.js
+++ src/Component/Pages/Scooter/Post/Component/ImageCarousel.js
@@ -1,67 +1,73 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 // import scooter from "../../Databse/ScooterData.json"
 // import { useMediaQuery } from 'react-responsive'
 const ImageCarousel = (props) => {
-    // const data = scooter.find((item) => item.id === 1)
+  // const data = scooter.find((item) => item.id === 1)
 
-const [showData , setShowData] = useState([])
+  const [showData, setShowData] = useState([]);
 
-const title = props.altText;
-useEffect(() => {
+  const title = props.altText;
+  useEffect(() => {
     setShowData(props.galleryData);
-}, [props.galleryData])
+  }, [props.galleryData]);
 
-    return (
-        <>
-            <div
-                id="carouselExampleFade"
-                className="carousel slide carousel-fade"
-                data-bs-ride="carousel"
-                style={{
-                    maxHeight: "350px",
-                }}
-            >
-                <div className="carousel-inner">
-                    {/* <div className="carousel-item active">
+  return (
+    <>
+      <div
+        id="carouselExampleFade"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+        style={{
+          maxHeight: "350px",
+        }}
+      >
+        <div className="carousel-inner">
+          {/* <div className="carousel-item active">
                         <img src="https://bd.gaadicdn.com/processedimages/tvs/iqube-electric/494X300/iqube-electric63368c7e20470.jpg?imwidth=400&impolicy=resize" className="d-block w-100" alt="..." />
                     </div> */}
-                    {showData.map((item,index) => {
-                        return (
-                            <>
-                                <div className={index ===0 ? "carousel-item active":"carousel-item"} key={index}>
-                                    <img src={item} className="d-block w-100" alt= {title}/>
-                                </div>
-                            </>
-                        )})}
-                    
-                 {/*    <div className="carousel-item">
+          {showData.map((item, index) => {
+            return (
+              <>
+                <div
+                  className={
+                    index === 0 ? "carousel-item active" : "carousel-item"
+                  }
+                  key={index}
+                >
+                  <img src={item} className="d-block w-100" alt={title} />
+                </div>
+              </>
+            );
+          })}
+
+          {/*    <div className="carousel-item">
                         <img src="https://bd.gaadicdn.com/processedimages/tvs/iqube-electric/494X300/iqube-electric63368c7ea0ee9.jpg?imwidth=400&impolicy=resize" className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
                         <img src="https://bd.gaadicdn.com/processedimages/tvs/iqube-electric/494X300/iqube-electric6285bdf0e536a.jpg?tr=w-290" className="d-block w-100" alt="..." />
                     </div> */}
-                </div>
-                <button
-                    className="carousel-control-prev"
-                    type="button"
-                    data-bs-target="#carouselExampleFade"
-                    data-bs-slide="prev"
-                >
-                    <span className="carousel-control-prev-icon" aria-hidden="true" />
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button
-                    className="carousel-control-next"
-                    type="button"
-                    data-bs-target="#carouselExampleFade"
-                    data-bs-slide="next"
-                >
-                    <span className="carousel-control-next-icon" aria-hidden="true" />
-                    <span className="visually-hidden">Next</span>
-                </button>
-            </div>
-        </>
-    )
-}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default ImageCarousel
+export default ImageCarousel;
