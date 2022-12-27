fbc2d18b47c2c462590c789acf2a32532e155a5c
Amit <183099@nith.ac.in>
2022-12-28 01:07:25 +0530

    add slider

--- src/Component/Pages/Scooter/Post/Component/SimilarScooter.js
+++ src/Component/Pages/Scooter/Post/Component/SimilarScooter.js
@@ -1,106 +1,111 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import scooter from "../../Databse/ScooterData.json";
+import Scooter from "../../Databse/ScooterData.json";
+import Slider from "react-slick";
+import { useNavigate } from "react-router-dom";
 const SimilarScooter = (props) => {
-  const valData = scooter.find((item) => item.path === props.name);
+  const data = Scooter.filter((item) => item.path !== props.name);
+  const navigate = useNavigate();
 
-  const showData = [
-    {
-      id: 1,
-      value: scooter.filter(
-        (item) => item.scootername === valData.similarScooter[0]
-      ),
-    },
-    {
-      id: 2,
-      value: scooter.filter(
-        (item) => item.scootername === valData.similarScooter[1]
-      ),
-    },
-    {
-      id: 3,
-      value: scooter.filter(
-        (item) => item.scootername === valData.similarScooter[2]
-      ),
-    },
-  ];
+  const setting = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    addpaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <>
-      <div className="container d-flex justify-content-center">
-        <div className="row">
-          <div className="col-lg-12 col-md-12 col-12 d-md-flex align-items-md-stretch">
-            <div className="count-box py-5">
-              <h1 className="text-center">
-                Similar Scooter to {props.name.replace(/-/g, " ")}
-              </h1>
-              <div className="row d-flex justify-content-center ">
-                {/* add three top scooter card */}
-                {showData.map((item) => {
-                  return (
-                    <>
-                      <div
-                        className="col-lg-4 col-md-6 d-flex mt-3 justify-content-center   text-center"
-                        key={item.id}
-                      >
-                        <div
-                          className="card shadow p-3 mb-5 bg-body rounded"
-                          style={{ width: "18rem" }}
-                        >
-                          <img
-                            src={item.value[0].image}
-                            className="card-img-top "
-                            alt={item.value[0].company}
-                            style={{
-                              height: "200px",
-                            }}
-                          />
-                          <div className="card-body">
-                            <Link to={`/scooter/${item.value[0].path}`}>
-                              <h5 className="card-title">
-                                {item.value[0].scootername}
-                              </h5>
-                            </Link>
-
-                            <div
-                              className="card-text"
-                              style={{
-                                fontWeight: "700",
-                                fontSize: "20px",
-                              }}
-                            >
-                              <span>&#8377;</span> {item.value[0].price}
-                              <div
-                                style={{
-                                  fontSize: "10px",
-                                  color: "grey",
-                                  display: "inline-block",
-                                  lineHeight: "0px",
-                                  marginLeft: "5px",
-                                }}
-                              >
-                                Estimated Price
-                              </div>
-                            </div>
-
-                            <p className="card-text text-black">
-                              {item.value[0].oneLine}
-                            </p>
-                            <Link
-                              to={`/scooter/${item.value[0].path}`}
-                              className="btn btn-primary"
-                            >
-                              Know More
-                            </Link>
-                          </div>
-                        </div>
+      <div className="container">
+        <Slider {...setting}>
+          {data.map((item) => {
+            return (
+              <>
+                <div
+                  key={item.id}
+                  style={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <div
+                    className="card mx-2"
+                    style={{
+                      maxWidth: "18rem",
+                      cursor: "pointer",
+                      transition: "all 0.3s ease-in-out",
+                      boxShadow: "0 0 11px rgba(33,33,33,.2)",
+                      borderRadius: "10px",
+                      border: "none",
+                      overflow: "hidden",
+                      marginBottom: "20px",
+                      marginTop: "20px",
+                      backgroundColor: "#fff",
+                    }}
+                    onClick={() => {
+                      navigate(`/scooter/${item.path}`);
+                      window.scrollTo(0, 0);
+                    }}
+                  >
+                    <img
+                      src={item.image}
+                      alt={item.scootername}
+                      className="card-img-top"
+                      style={{
+                        height: "180px",
+                      }}
+                    />
+                    <div className="card-body">
+                      <div className="card-title text-center">
+                        {item.scootername}
                       </div>
-                    </>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-        </div>
+                      <div
+                        className="card-text text-center"
+                        style={{
+                          color: "brown",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        Rs. {item.price}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </Slider>
       </div>
     </>
   );
