0b84db8c268034c44cd3d288261427a1d15e3303
Amit <183099@nith.ac.in>
2023-01-05 00:04:10 +0530

    change background color

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,57 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-
   const showNav = [
     {
       id: 1,
       name: "Home",
-      path: "/"
+      path: "/",
     },
     {
       id: 2,
       name: "Scooter",
-      path: "/scooter"
+      path: "/scooter",
     },
     {
       id: 3,
       name: "Blog",
-      path: "/blog"
+      path: "/blog",
     },
     {
       id: 4,
       name: "privacy",
-      path: "/privacy-policy"
+      path: "/privacy-policy",
     },
     {
       id: 5,
       name: "terms and conditions",
-      path: "/terms-and-conditions"
-    }
-  ]
+      path: "/terms-and-conditions",
+    },
+  ];
   return (
     <>
-      <div className="container">
-        <footer className="py-3 my-4">
-          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-            {showNav.map((nav) => {
-              return (
-                <li className="nav-item" key={nav.id}>
-                  <Link to={nav.path} className="nav-link px-2 text-muted">
-                    {nav.name}
-                  </Link>
-                </li>
-              )
-            })
-            }
-          </ul>
-          <p className="text-center text-muted">© 2022 Company, Inc</p>
-        </footer>
+      <div
+        style={{
+          backgroundColor: "#f5f5f5",
+        }}
+      >
+        <div className="container">
+          <footer className="py-5 ">
+            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+              {showNav.map((nav) => {
+                return (
+                  <li className="nav-item" key={nav.id}>
+                    <Link to={nav.path} className="nav-link px-2 text-muted">
+                      {nav.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            <p className="text-center text-muted">© 2022 Company, Inc</p>
+          </footer>
+        </div>
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
