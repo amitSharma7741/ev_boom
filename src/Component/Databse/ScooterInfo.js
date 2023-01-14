b8b639bf1b23d81817da11dc8a0a90c2e6fb8e5a
Amit <183099@nith.ac.in>
2023-01-14 23:00:22 +0530

    initial commit

---
+++ src/Component/Databse/ScooterInfo.js
@@ -0,0 +1,33 @@
+export const ScooterInfo = [
+    {
+        id: 1,
+        scootername:"Ola s1 pro",
+        company:"Ola",
+        shortDescription:"Ola s1 pro  ",
+        price: "1,30,000",
+        image: "https://www.olacabs.com/images/ola-s1-pro.png",
+        rating: 4.5,
+        reviews: 100,
+        topSpeed: "116 Kmph",
+        araiRange: "181 Kms",
+        weight: "130 Kg",
+        batteryCapacity: "3.97 kWh",
+        chargingTime: "6 Hours 30 Minutes",
+        batteryType: "Lithium-ion Battery",
+        extraSpecifications: [
+            {
+                Acceleration: "0-40 Kmph in 2.9 Seconds"
+                
+            }
+        ],
+        tags: ["Ola", "Scooter", "125cc", "Electric"],
+        gallery: [
+            "https://www.olacabs.com/images/ola-s1-pro.png",
+            "https://www.olacabs.com/images/ola-s1-pro.png"
+        ],
+
+        longDescription:"OLA S1 Pro is an electric scooter available at a starting price of Rs. 1,29,999 in India. It is available in only 1 variant and 11 colours. OLA S1 Pro generates 5500 W power from its motor. With both front and rear disc brakes, OLA S1 Pro comes up with combined braking system of both wheels.\n\nThe Ola S1 Pro is the premium variant of the S1 electric scooter range from Ola Electric. This variant of the S1 range packs higher specifications and features more colour options than the standard S1 electric scooter.\n\nThe electric motor on the S1 Pro is tuned to deliver 8.5kW of maximum power and 58Nm of peak torque. The company promises a top speed of 115kmph and claims a range of 181km per full charge as against 90kmph and 121km on the base model. The 3.97kWh battery pack can be fast charged for a range of 75km in 18 minutes. The full charging time is rated at six and a half hours.\n\nThe feature list, similar to the base model, includes a twin-pod headlight at the front, apron-mounted sleek LED indicators, a body-coloured front fender, curvy side panels, a sleek LED taillight, an external charging port at the back, and a split-style pillion grabrail. The scooter features a 36-litre under-seat storage space that can accommodate two open-face helmets. The colour palette for the Pro variant includes 10 options.\n\nStandard features on both models include proximity lock/unlock, remote boot lock, call alert, message alert, infotainment, side-stand alert, anti-theft alert, geo-fencing, Wi-Fi, Bluetooth, GPS, onboard navigation, limp home mode, reverse mode, get-home mode, take-me-home lights, find my scooter, HMI moods with sound, electronic steering lock, HMI brightness adjuster, welcome screen, OTA updates, manual SOS, and predictive maintenance. The Pro variant further benefits from hill-hold system, cruise control, voice assist, and three ride modes (Normal, Sport, and Hyper).\n\nThe hardware is similar on both variants. Thus, the suspension tasks are handled by a single front fork, a rear mono-shock, disc brakes on both wheels, and a combined braking system" 
+ 
+
+    }
+]
