4bb486044021083566bdc8e10f0c140c7bda0129
Amit <183099@nith.ac.in>
2023-01-17 21:46:56 +0530

    use md file

--- src/Component/Pages/Blog/BlogPost.js
+++ src/Component/Pages/Blog/BlogPost.js
@@ -2,63 +2,50 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BlogsData } from "./BlogsData";
 import ReactMarkdown from "react-markdown";
+import ReactGA from "react-ga4";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 import "./CSS/BlogPost.css";
 import { useMediaQuery } from "react-responsive";
 import Seo from "../../SEO/Seo";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import Author from "./Component/Author/Author";
-import { MdFacebook } from "react-icons/md"
-import { FaWhatsapp, FaTwitter } from "react-icons/fa"
-import { SiGooglenews } from "react-icons/si"
-import { ExternalLink } from 'react-external-link';
-import { Player } from "@lottiefiles/react-lottie-player";
+import Sharethispage from "./Component/ShareThisPage/Sharethispage";
 const BlogPost = () => {
   const { blogpost } = useParams();
 
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
   const [readTime, setReadTime] = useState(1);
+  const [postContent, setPostcontent] = useState("");
 
   const blog = BlogsData.filter((blog) => blog.path === blogpost);
   const urlParam = `/blog/${blogpost}`;
-  const fullUrl = `https://evstart.netlify.app${urlParam}`
-
-  const twitterUrl = `https://twitter.com/share?url=${fullUrl}`
-  const whatsAppUrl = `whatsapp://send?text=${blog[0]?.description.slice(0, 150)}.....\n\n${fullUrl}`
-  const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${fullUrl}`
-  const googleNewsUrl = ""
-
-
+  const fullUrl = `https://evstart.netlify.app${urlParam}`;
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    const text = blog[0]?.description;
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+    import(`./MarkdownFile/${blogpost}.md`).then((res) => {
+      fetch(res.default)
+        .then((response) => response.text())
+        .then((response) => setPostcontent(response))
+        .catch((err) => console.log(err));
+    });
+    const text = postContent;
     const wordsPerMinute = 200;
     const noOfWords = text.split(/\s/g).length; // split by space
     const minutes = noOfWords / wordsPerMinute;
     const readTime = Math.ceil(minutes); // round up
     setReadTime(readTime);
-    /*  // change table tag class name
-        const table = document.querySelector("table")
-        table.classList.add("table table-striped text-successtable-border border-light")
-        // change threr tag class name
-        const thead = document.querySelector("thead")
-        thead.classList.add("border-light")
-        // change  th tag  attribute scope 
-        const th = document.querySelectorAll("th")
-        th.forEach((item) => {
-        })
-        item.setAttribute("scope", "row") */
-
-  }, [blog]);
+  }, [blog, blogpost, postContent]);
 
   return (
     <>
       {/* add seo */}
       <Seo
         title={blog[0]?.title}
-        description={blog[0]?.description.slice(0, 150)}
+        description={postContent.slice(0, 150)}
         image={blog[0]?.img}
         url={urlParam}
       />
@@ -89,26 +76,22 @@ const BlogPost = () => {
             >
               <div className="d-flex justify-content-between">
                 <div>
-
-              <Author
-                author={blog[0]?.author}
-                authorImg={blog[0]?.authorImg}
-                date={blog[0]?.date}
-                title={blog[0]?.title}
-              />
+                  <Author
+                    author={blog[0]?.author}
+                    authorImg={blog[0]?.authorImg}
+                    date={blog[0]?.date}
+                    title={blog[0]?.title}
+                  />
                 </div>
-                <div className="d-flex justify-content-center mt-3" >
-                  <Player
-                  src = "https://assets8.lottiefiles.com/packages/lf20_kblbXu.json"
-                  autoplay
-                  loop
-                  speed={0.5}
-                  style={{
-                    width:"20px",
-                    height:"20px"
-                  }}
-/>
-                <p className="" style={{fontSize:"16px"}}>{readTime} min read</p>
+                <div className="d-flex justify-content-center mt-3">
+                  <p
+                    className=""
+                    style={{
+                      fontSize: isMobile ? "13px" : "15px",
+                    }}
+                  >
+                    {readTime} min read
+                  </p>
                 </div>
               </div>
 
@@ -117,66 +100,14 @@ const BlogPost = () => {
                 style={{
                   color: "black",
                   fontWeight: "bold",
-                  fontSize: isMobile ? "30px" : "40px",
+                  fontSize: isMobile ? "20px" : "40px",
                   textAlign: "justify",
                 }}
               >
                 {blog[0]?.title}
               </h1>
-              <div className="d-flex justify-content-center mt-3">
-                <div className="d-flex align-items-center ">
-                  {/*  social media icons */}
-                  <div>
-                    <ExternalLink href={facebookUrl}>
 
-                      <MdFacebook style={{
-                        color: "blue",
-                        fontSize: "30px",
-                        cursor: "pointer"
-                      }}
-
-                      />
-                    </ExternalLink>
-                    <ExternalLink href={whatsAppUrl}>
-                      <FaWhatsapp style={{
-                        color: "green",
-                        fontSize: "30px",
-                        marginLeft: "10px",
-                        cursor: "pointer"
-                      }}
-                      />
-                    </ExternalLink>
-                    <ExternalLink href={twitterUrl}>
-                      <FaTwitter style={{
-                        color: "blue",
-                        fontSize: "30px",
-                        margin: "0 10px",
-                        cursor: "pointer"
-                      }}
-                      />
-                    </ExternalLink>
-                  </div>
-                </div>
-                <div className="d-flex align-items-center ">
-                  <ExternalLink href={googleNewsUrl}>
-                    <div style={{
-                      border: "1px solid black",
-                      borderRadius: "15px",
-                      padding: "5px 6px",
-                      background: "rgba(80,171,241,.2)",
-
-                    }} >
-                      <SiGooglenews style={{
-                        color: "blue",
-                        fontSize: "20px"
-                      }} /> <span style={{ 
-                        textDecoration: "none", color: "black" }}>Follow Us</span>
-                    </div>
-                  </ExternalLink>
-                </div>
-
-              </div>
-
+              <Sharethispage url={fullUrl} desc={postContent.slice(0, 150)} />
 
               <hr />
 
@@ -189,8 +120,9 @@ const BlogPost = () => {
                 }}
               >
                 <ReactMarkdown
-                  children={blog[0]?.description}
+                  children={postContent}
                   remarkPlugins={[remarkGfm]}
+                  rehypePlugins={[rehypeRaw]} // for html in md
                   linkTarget="_blank"
                   //  lazy load image
                   components={{
@@ -199,6 +131,7 @@ const BlogPost = () => {
                         src={props.src}
                         alt={props.alt}
                         effect="blur"
+                        className="img-fluid"
                       />
                     ),
                     // blackquote tag style text wrap
@@ -220,6 +153,9 @@ const BlogPost = () => {
                   }}
                 />
               </div>
+              <hr />
+              <Sharethispage url={fullUrl} desc={postContent.slice(0, 150)} />
+              <hr />
             </div>
           </div>
         </div>
