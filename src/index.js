071759bcaf48220fc86f929d41da91fe9731a059
Amit <183099@nith.ac.in>
2023-01-17 21:40:14 +0530

    change react-ga to react-ga4

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter  } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga4";
 import {  HelmetProvider } from 'react-helmet-async';
 
 
-ReactGA.initialize('UA-250993129-1');
+ReactGA.initialize('G-47SCW9BW0Y');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
