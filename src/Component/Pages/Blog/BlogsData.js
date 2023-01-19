69b776a6abea0767f4b8499eb2668a31528cd35f
Amit <183099@nith.ac.in>
2023-01-19 13:36:16 +0530

    add new blog

--- src/Component/Pages/Blog/BlogsData.js
+++ src/Component/Pages/Blog/BlogsData.js
@@ -9,8 +9,7 @@ export const BlogsData = [
     author: "Jon Snow",
     authorImg:
       "https://images.unsplash.com/photo-1515175704145-8a06ffce6b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=596&q=80",
-    category: "Electric Scooter",
-    link: "ola-electric-scooter",
+    category: "Electric Scooter", 
   },
   {
     Id: "2",
@@ -22,8 +21,7 @@ export const BlogsData = [
     author: "Jon Snow",
     authorImg:
       "https://images.unsplash.com/photo-1515175704145-8a06ffce6b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=596&q=80",
-    category: "Electric Scooter",
-    link: "electric-scooter-vs-petrol-scooter",
+    category: "Electric Scooter", 
   },
   {
     Id: "3",
@@ -34,7 +32,17 @@ export const BlogsData = [
     date: "07 jan 2023",
     author: "Alex Guill",
     authorImg:"https://images.unsplash.com/photo-1552915170-73c2330ae617?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
-    category: "Electric Scooter",
-    link: "how-to-choose-the-best-electric-scooter",
+    category: "Electric Scooter", 
   },
+  {
+    Id: "4",
+    title: "Different Types of EV Charging Stations and How to Use Them",
+    path: "different-types-of-ev-charging-stations-and-how-to-use-them",  
+    img: "https://img.freepik.com/free-photo/plugged-chargers-into-two-electric-cars-charge-station_1268-17703.jpg?w=1060&t=st=1674033618~exp=1674034218~hmac=10f70003e7dc97d45b190806f65890d44c29c4159fbe107d9dce5e9d58a2bae9",
+    description:"If you’re an electric vehicle (EV) owner, you’re probably familiar with the need to charge your car. To do so, you’ll need access to an EV charging station. But what types of charging stations are available? How do you use them? And what else do you need to know? This article will answer all of these questions and more, and provide a comprehensive guide to EV charging stations.",
+    date: "19 jan 2023",
+    author: "Seth Doyle",
+    authorImg:"https://images.unsplash.com/photo-1595760780346-f972eb49709f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHByb2Zlc3Npb25hbCUyMGdpcmx8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    category: "Electric Scooter", 
+  } 
 ];
