9bb78430f67154767aa027be7ee72d80b0ed96d0
Amit <183099@nith.ac.in>
2023-01-21 12:56:29 +0530

    reverse the blogs data

--- src/Component/Pages/Blog/BlogsData.js
+++ src/Component/Pages/Blog/BlogsData.js
@@ -1,48 +1,80 @@
 export const BlogsData = [
   {
-    Id: "1",
-    title: "Electric Scooter sales data in nov 2022",
-    path: "electric-scooter-sales-data-in-nov-2022",
-    img: "https://images.unsplash.com/photo-1642665076339-ca50c80a44e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-    description:"An electric vehicle (EV) is a type of vehicle that is powered by an electric motor, rather than a gasoline engine. EV technology has been around for over a century, but it has gained significant popularity in recent years due to concerns about climate change and air pollution. EVs are generally considered to be more environmentally friendly than gasoline-powered vehicles, as they produce zero emissions from their tailpipes and have a lower overall environmental impact during their lifecycle.",
-    date: "25 dec 2022",
-    author: "Jon Snow",
+    Id: "6",
+    title: "How to maximizing efficiency and minimizing charging time of electric vehicle",
+    path: "how-to-maximizing-efficiency-and-minimizing-charging-time-of-electric-vehicle",
+    img: "https://images.unsplash.com/photo-1605919993579-457ae8e08a20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGdpcmwlMjB3aXRoJTIwY2FyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    description:
+      "Electric vehicles (EVs) are becoming increasingly popular as more people look to reduce their carbon footprint and save money on fuel costs. However, one of the biggest challenges with EVs is maximizing efficiency and minimizing charging time. In this blog post, we will discuss several strategies that can help you get the most out of your EV and charge it as quickly as possible.",
+    date: "21 jan 2023",
+    author: "Seth Doyle",
     authorImg:
-      "https://images.unsplash.com/photo-1515175704145-8a06ffce6b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=596&q=80",
-    category: "Electric Scooter", 
+      "https://images.unsplash.com/photo-1595760780346-f972eb49709f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHByb2Zlc3Npb25hbCUyMGdpcmx8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    category: "Electric Scooter",
   },
   {
-    Id: "2",
-    title: "Why electric scooter is better than petrol scooter",
-    path: "why-electric-scooter-is-better-than-petrol-scooter",
-    img: "https://images.unsplash.com/photo-1566936737687-8f392a237b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    description:"Electric scooters are becoming an increasingly popular transportation option due to their convenience, cost-effectiveness, and environmental benefits. When compared to petrol scooters, electric scooters offer many advantages that make them a better choice for many people.",
-    date: "04 jan 2023",
-    author: "Jon Snow",
+    Id: "5",
+    title: "How to find and locate charging stations in india",
+    path: "how-to-find-and-locate-charging-stations-in-india",
+    img: "https://images.unsplash.com/photo-1554175940-c7d7ede2ecb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTJ8fEV2JTIwY2hhcmdpbmclMjBzdGF0aW9uc3xlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
+    description:
+      "Electric vehicles (EVs) have been gaining popularity in India in recent years, thanks to the government's push for cleaner and more sustainable transportation. However, one of the main concerns for EV owners is the availability and accessibility of charging stations. Finding a charging station can be a challenge, especially when traveling long distances. In this blog post, we will discuss how to find and locate charging stations in India.",
+    date: "21 jan 2023",
+    author: "Seth Doyle",
     authorImg:
-      "https://images.unsplash.com/photo-1515175704145-8a06ffce6b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=596&q=80",
-    category: "Electric Scooter", 
+      "https://images.unsplash.com/photo-1595760780346-f972eb49709f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHByb2Zlc3Npb25hbCUyMGdpcmx8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    category: "Electric Scooter",
+  },
+  {
+    Id: "4",
+    title: "Different Types of EV Charging Stations and How to Use Them",
+    path: "different-types-of-ev-charging-stations-and-how-to-use-them",
+    img: "https://img.freepik.com/free-photo/plugged-chargers-into-two-electric-cars-charge-station_1268-17703.jpg?w=1060&t=st=1674033618~exp=1674034218~hmac=10f70003e7dc97d45b190806f65890d44c29c4159fbe107d9dce5e9d58a2bae9",
+    description:
+      "If you’re an electric vehicle (EV) owner, you’re probably familiar with the need to charge your car. To do so, you’ll need access to an EV charging station. But what types of charging stations are available? How do you use them? And what else do you need to know? This article will answer all of these questions and more, and provide a comprehensive guide to EV charging stations.",
+    date: "19 jan 2023",
+    author: "Seth Doyle",
+    authorImg:
+      "https://images.unsplash.com/photo-1595760780346-f972eb49709f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHByb2Zlc3Npb25hbCUyMGdpcmx8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    category: "Electric Scooter",
   },
   {
     Id: "3",
     title: "Ather 450x free non copyright images download 2023",
     path: "ather-450x-free-non-copyright-images-download-2023",
     img: "https://images.unsplash.com/photo-1624609083513-2ce2a93af609?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    description:"An extensive and free collection of images of the Ather 450x electric scooter is available to be used for various purposes. Whether it is for personal use, a website, or even for commercial use, you can find an image in this collection that will suit your needs. With its vast selection, you can find an image that will make your project stand out from the rest.",
+    description:
+      "An extensive and free collection of images of the Ather 450x electric scooter is available to be used for various purposes. Whether it is for personal use, a website, or even for commercial use, you can find an image in this collection that will suit your needs. With its vast selection, you can find an image that will make your project stand out from the rest.",
     date: "07 jan 2023",
     author: "Alex Guill",
-    authorImg:"https://images.unsplash.com/photo-1552915170-73c2330ae617?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
-    category: "Electric Scooter", 
+    authorImg:
+      "https://images.unsplash.com/photo-1552915170-73c2330ae617?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    category: "Electric Scooter",
   },
   {
-    Id: "4",
-    title: "Different Types of EV Charging Stations and How to Use Them",
-    path: "different-types-of-ev-charging-stations-and-how-to-use-them",  
-    img: "https://img.freepik.com/free-photo/plugged-chargers-into-two-electric-cars-charge-station_1268-17703.jpg?w=1060&t=st=1674033618~exp=1674034218~hmac=10f70003e7dc97d45b190806f65890d44c29c4159fbe107d9dce5e9d58a2bae9",
-    description:"If you’re an electric vehicle (EV) owner, you’re probably familiar with the need to charge your car. To do so, you’ll need access to an EV charging station. But what types of charging stations are available? How do you use them? And what else do you need to know? This article will answer all of these questions and more, and provide a comprehensive guide to EV charging stations.",
-    date: "19 jan 2023",
-    author: "Seth Doyle",
-    authorImg:"https://images.unsplash.com/photo-1595760780346-f972eb49709f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHByb2Zlc3Npb25hbCUyMGdpcmx8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    category: "Electric Scooter", 
-  } 
+    Id: "2",
+    title: "Why electric scooter is better than petrol scooter",
+    path: "why-electric-scooter-is-better-than-petrol-scooter",
+    img: "https://images.unsplash.com/photo-1566936737687-8f392a237b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    description:
+      "Electric scooters are becoming an increasingly popular transportation option due to their convenience, cost-effectiveness, and environmental benefits. When compared to petrol scooters, electric scooters offer many advantages that make them a better choice for many people.",
+    date: "04 jan 2023",
+    author: "Jon Snow",
+    authorImg:
+      "https://images.unsplash.com/photo-1515175704145-8a06ffce6b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=596&q=80",
+    category: "Electric Scooter",
+  },
+  {
+    Id: "1",
+    title: "Electric Scooter sales data in nov 2022",
+    path: "electric-scooter-sales-data-in-nov-2022",
+    img: "https://images.unsplash.com/photo-1642665076339-ca50c80a44e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    description:
+      "An electric vehicle (EV) is a type of vehicle that is powered by an electric motor, rather than a gasoline engine. EV technology has been around for over a century, but it has gained significant popularity in recent years due to concerns about climate change and air pollution. EVs are generally considered to be more environmentally friendly than gasoline-powered vehicles, as they produce zero emissions from their tailpipes and have a lower overall environmental impact during their lifecycle.",
+    date: "25 dec 2022",
+    author: "Jon Snow",
+    authorImg:
+      "https://images.unsplash.com/photo-1515175704145-8a06ffce6b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=596&q=80",
+    category: "Electric Scooter",
+  },
 ];
