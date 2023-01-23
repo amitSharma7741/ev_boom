68ff0bfacbfd05959baf55c718421f7a4cf67f4a
Amit <183099@nith.ac.in>
2023-01-23 15:04:40 +0530

    pretify

--- src/Component/Pages/Home/Component/CompareTwo/CompareTwo.js
+++ src/Component/Pages/Home/Component/CompareTwo/CompareTwo.js
@@ -5,44 +5,43 @@ import Scooter from "../../../../Databse/ScooterData.json";
 import { useMediaQuery } from "react-responsive";
 import { useNavigate } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-const CompareTwo = () => { 
+const CompareTwo = () => {
   const navigate = useNavigate();
 
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
 
   const first1 = Scooter.filter((item) => item.id < 3);
-  const first2 = Scooter.filter((item) => item.id < 5 && item.id > 2)
-  const first3 =  Scooter.filter((item) => item.id < 7 && item.id > 4)
-  const first4 = Scooter.filter((item) => item.id < 9 && item.id > 6)
+  const first2 = Scooter.filter((item) => item.id < 5 && item.id > 2);
+  const first3 = Scooter.filter((item) => item.id < 7 && item.id > 4);
+  const first4 = Scooter.filter((item) => item.id < 9 && item.id > 6);
   const data = [
     {
       id: 1,
       name: "Scooter",
       value: first1,
-      comare:`${first1[0].scootername} vs ${first1[1].scootername}`
+      comare: `${first1[0].scootername} vs ${first1[1].scootername}`,
     },
     {
       id: 2,
       name: "Scooter",
       value: first2,
-      comare:`${first2[0].scootername} vs ${first2[1].scootername}`
+      comare: `${first2[0].scootername} vs ${first2[1].scootername}`,
     },
     {
       id: 3,
       name: "Scooter",
-      value:first3,
-      comare:`${first3[0].scootername} vs ${first3[1].scootername}`
+      value: first3,
+      comare: `${first3[0].scootername} vs ${first3[1].scootername}`,
     },
     {
       id: 4,
       name: "Scooter",
-      value:first4,
-      comare:`${first4[0].scootername} vs ${first4[1].scootername}`
-    }
-  ]
- 
+      value: first4,
+      comare: `${first4[0].scootername} vs ${first4[1].scootername}`,
+    },
+  ];
 
-  const settings = { 
+  const settings = {
     // dots: true,
     // arrows: false,
     infinite: true,
@@ -84,171 +83,158 @@ const CompareTwo = () => {
     para: {
       fontSize: isMobile ? "10px" : "13px",
     },
-    val:{
+    val: {
       fontSize: isMobile ? "10px" : "13px",
       color: "brown",
       fontWeight: "bold",
-    }
+    },
   };
   return (
     <div className="container mt-3 shadow py-4">
-      <h3 className=" container text-start mb-4">Popular Electric Scooters Comparison</h3>
+      <h3 className=" container text-start mb-4">
+        Popular Electric Scooters Comparison
+      </h3>
       <Slider {...settings}>
         {data.map((item) => {
           return (
             <>
-              <div   key={item.id}>
+              <div key={item.id}>
                 <div
                   className="container "
-                
                   style={{
                     display: "flex",
                     justifyContent: "center",
                   }}
                 >
                   <div
-                    className="row  shadow-sm bg-body pb-3 w-100"
+                    className="card shadow-sm bg-body pb-3 w-100"
                     style={{
                       // border: "1px solid black",
-                      borderRadius: "10px", 
+                      borderRadius: "10px",
                       padding: "10px",
-                      position:"relative"
-                      
+
                       //   height: "300px",
                     }}
                   >
-                    {item.value.map((elem) => {
-                      return (
-                        <>
-                          <div className="col" key={elem.id}>
-                            <div
-                              className="card mb-3"
-                              style={{
-                                width: "100%",
-                                border: "none",
-                              }}
-                            >
-                              <LazyLoadImage
-                                src={elem.image}
-                                className="card-img-top"
-                                alt={elem.scootername}
-                                style={{
-                                  height: "150px",
-                                }}
-                                effect="blur"
-                              />
-                              <div className="card-body">
-                                <h5
-                                  className="card-title"
+                    <div className="card-body">
+                      <div className="row">
+                        {item.value.map((elem) => {
+                          return (
+                            <>
+                              <div className="col-6" key={elem.id}>
+                                <div
+                                  className="card "
                                   style={{
-                                    textAlign: "center",
-                                    fontSize: isMobile ? "12px" : "16px",
-                                    fontWeight: "bold",
-                                    cursor: "pointer",
+                                    width: "100%",
+                                    border: "none",
                                   }}
-                                  onClick={() => navigate(`/scooter/${elem.path}`)}
-
                                 >
-                                  {elem.scootername}
-                                </h5>
-                                {/* <p className="card-text mb-4" style={styles.para}>
+                                  <LazyLoadImage
+                                    src={elem.image}
+                                    className="card-img-top"
+                                    alt={elem.scootername}
+                                    style={{
+                                      height: "150px",
+                                    }}
+                                    effect="blur"
+                                  />
+                                  <div className="card-body">
+                                    <h5
+                                      className="card-title"
+                                      style={{
+                                        textAlign: "center",
+                                        fontSize: isMobile ? "12px" : "16px",
+                                        fontWeight: "bold",
+                                        cursor: "pointer",
+                                      }}
+                                      onClick={() =>
+                                        navigate(`/scooter/${elem.path}`)
+                                      }
+                                    >
+                                      {elem.scootername}
+                                    </h5>
+                                    {/* <p className="card-text mb-4" style={styles.para}>
                                   Price :{" "}
                                   <span
                                     style={{
                                       color: "brown",
                                       fontWeight: "bold",
-                                    }}
+                                    }} 
                                   >
                                     {elem.price}
                                   </span>
                                 </p> */}
-                                <div className="row">
-                                  <div className="col-12">
-                                    <p className ="float-start" style={styles.para}>
-                                      Price 
-                                    </p>
-                                    <p className="float-end" style={styles.val}>
-                                      {elem.price}
-                                    </p>
+                                    <div className="row">
+                                      <div className="col-12">
+                                        <p
+                                          className="float-start"
+                                          style={styles.para}
+                                        >
+                                          Price
+                                        </p>
+                                        <p
+                                          className="float-end"
+                                          style={styles.val}
+                                        >
+                                          {elem.price}
+                                        </p>
+                                      </div>
+                                    </div>
+                                    <div className="row">
+                                      <div className="col-12">
+                                        <p
+                                          className="float-start"
+                                          style={styles.para}
+                                        >
+                                          Range
+                                        </p>
+                                        <p
+                                          className="float-end"
+                                          style={styles.val}
+                                        >
+                                          {elem.araiRange}
+                                        </p>
+                                      </div>
+                                    </div>
+                                    <div className="row">
+                                      <div className="col-12">
+                                        <p
+                                          className="float-start"
+                                          style={styles.para}
+                                        >
+                                          Speed
+                                        </p>
+                                        <p
+                                          className="float-end"
+                                          style={styles.val}
+                                        >
+                                          {elem.topSpeed}
+                                        </p>
+                                      </div>
+                                    </div>
                                   </div>
                                 </div>
-                                <div className="row">
-                                  <div className="col-12">
-                                    <p className ="float-start" style={styles.para}>
-                                      Range
-                                    </p>
-                                    <p className="float-end" style={styles.val}>
-                                      {elem.araiRange}
-                                    </p>
-                                  </div>
-                                </div>
-                                <div className="row">
-                                  <div className="col-12">
-                                    <p className ="float-start" style={styles.para}>
-                                    Speed
-                                    </p>
-                                    <p className="float-end" style={styles.val}>
-                                    {elem.topSpeed}
-                                    </p>
-                                  </div>
-                                </div>
-                                {/* <div className="card-text w-100" style={styles.para}> 
-                                    <p className="float-start" >
-                                      Price
-                                    </p>
-                                    <p className=" float-end" style={{
-                                      color: "brown",
-                                      fontWeight: "bold",
-                                    }}>
-                                      {elem.price}
-                                    </p> 
-                                </div> */}
-                                {/* <div className="card-text w-100" style={styles.para}> 
-                                    <p className="float-start" >
-                                      Range
-                                    </p>
-                                    <p className=" float-end" style={{
-                                      color: "brown",
-                                      fontWeight: "bold",
-                                    }}>
-                                      {elem.araiRange}
-                                    </p> 
-                                </div>
-                                <div className="card-tex w-100t" style={styles.para}> 
-                                    <p className="float-start" >
-                                      Top Speed
-                                    </p>
-                                    <p className=" float-end" style={{
-                                      color: "brown",
-                                      fontWeight: "bold",
-                                    }}>
-                                      {elem.topSpeed}
-                                    </p> 
-                                </div> */}
-                                {/* <p className="card-text" style={styles.para}>
-                                  Top Speed : {elem.topSpeed}
-                                </p> */}
                               </div>
-                            </div>
-                          </div>
-                        </>
-                      );
-                    })}
-                    <div style={{
-                      position:"absolute",
-                      bottom:"0",
-                      display:"flex",
-                      justifyContent:"center",
-                      
-                    }}>
-                      <button className="btn btn-primary mb-2 " 
-                      onClick={()=>{
-                        navigate(`/compare/${item.comare.toLowerCase().replace(/ /g, "-")}`)
-                      }}
-                      >
-                         {item.comare}
-                      </button>
+                            </>
+                          );
+                        })}
+                      </div>
                     </div>
+                  
+                  <div className="card-footer d-flex justify-content-center">
+                    <button
+                      className="btn btn-primary mb-2 "
+                      onClick={() => {
+                        navigate(
+                          `/compare/${item.comare
+                            .toLowerCase()
+                            .replace(/ /g, "-")}`
+                        );
+                      }}
+                    >
+                      {item.comare}
+                    </button>
+                  </div>
                   </div>
                 </div>
               </div>
