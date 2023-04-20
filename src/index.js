82dac3e98572336f873d3f8cc7ad31a709bd5fb1
Amit <183099@nith.ac.in>
2023-04-20 22:27:38 +0530

    remove server side rendering

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-/* import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter  } from 'react-router-dom';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 import ReactGA from "react-ga4";
-import {  HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from "react-helmet-async";
 
 const GA4ID = process.env.REACT_APP_GA4_ID;
 ReactGA.initialize(GA4ID);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <HelmetProvider>
-      <App />
+        <App />
       </HelmetProvider>
     </BrowserRouter>
   </React.StrictMode>
@@ -25,15 +25,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
- */
-
-import React from 'react';
-import { createRoot } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-
-createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>
-);
