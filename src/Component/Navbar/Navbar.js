c1eeff73704f52576934f052d2b06c2e9c2d8a41
Amit <183099@nith.ac.in>
2023-09-29 22:32:35 +0530

    update navbar

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,165 +1,102 @@
-import React from 'react'
-import "./Navbar.css"
-import { Link, Outlet } from "react-router-dom"
-import Footer from '../Footer/Footer';
+import React from "react";
+// import "./Navbar.css";
+import { Link, Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 // import Logo from './Logo/Logo';
 
 const Navbar = () => {
-
   const showNav = [
     {
       id: 1,
       name: "Home",
-      path: "/"
+      path: "/",
     },
     {
       id: 2,
       name: "Scooter",
-      path: "/scooter"
+      path: "/scooter",
     },
     {
       id: 3,
-      name: "Blog",
-      path: "/blog"
-    }
-    /* {
-      id: 4,
-      name: "privacy policy",
-      path: "/privacy-policy"
-    } */
-  ]
-  //  if scroll down, navbar background color changes
+      name: "Blogs",
+      path: "/blog",
+    },
+  ];
 
-  /*  const scrollFunction = () => {
-     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-       document.getElementById("navbar").style.backgroundColor = "black";
-     } else {
-       document.getElementById("navbar").style.backgroundColor = "transparent";
-     }
-   }
-   window.onscroll = function () {
-     scrollFunction()
-   }; */
-  /* 
-  if we are not in home page than navbar backgroud color is 
-  background: linear-gradient(60deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%); 
-  and in home page navbar background color is transparent
-  */
-  /*    const path = window.location.pathname;
-      if(path !== "/"){
-        document.getElementById("navbar").style.backgroundColor = "transparent";
-      }
-      else {
-        document.getElementById("navbar").style.backgroundColor = "linear-gradient(60deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%)";
-      } */
-
-
-
-  /* const styles = {
-    text :{
-      color: "white",
-      textDecoration: "none",
-      // text in uppercase
-      textTransform: "uppercase",
-
-    }
-  } */
   return (
     <>
-      {/* 
-<nav className="navbar navbar-expand-sm     fixed-top" style={{
-  backgroundColor:  "transparent"
-}}>
-  <div className="container-fluid">
-  <ul className="navbar-nav">
-  <li className="nav-item">
-    <Link className="nav-link active" href="/">
-      Active
-    </Link>
-  </li>
-  <li className="nav-item">
-    <Link className="nav-link" href="/">
-      Link
-    </Link>
-  </li>
-  <li className="nav-item">
-    <Link className="nav-link" href="/">
-      Link
-    </Link>
-  </li>
-  <li className="nav-item">
-    <Link className="nav-link d " href="/">
-      Disabled
-    </Link>
-  </li>
-</ul>
-
-  </div>
-</nav> */}
-
-
-
-
-
-
-
-
-
-
-      <div className="top" id='navbar' style={{
-        backgroundColor: "transparent",
-        backgroundImage: " linear-gradient(60deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%)",
-      }}>
-
-        <div className="topCenter">
-          <ul className="topList">
-            {/* <li className=" " style={{
-              // backgroundColor: "white",
-            }}>
-              <Logo/>
-            </li> */}
-            {showNav.map((item) => {
-              return (
-                <li className="" key={item.id}>
-                  <Link className="" to={item.path} style={{
-                    color: "white",
-                    textDecoration: "none",
-                    // text in uppercase
-                    textTransform: "uppercase",
-                    margin: "0 10px",
-                    fontSize: "20px",
-                    fontWeight: "bold",
-
-
-                  }} >
-                    {item.name}
-                  </Link>
-                </li>
-              )
-            })}
-             
-            {/* <li className="topListItem">
-              <Link className="link" to="/post">
-                Post
-              </Link>
-            </li>  */}
-          </ul>
-        </div>
-        <div className="topRight">
-
-          <i className="topSearchIcon fas fa-search"></i>
-        </div>
+      <div
+        className=""
+        style={{
+          backgroundColor: "white",
+          position: "fixed",
+          width: "100%",
+          zIndex: "100",
+          top: "0",
+        }}
+      >
+        <nav className="navbar navbar-expand-lg bg-white">
+          <div className="container">
+            <Link
+              className="navbar-brand"
+              href="/"
+              style={{
+                fontWeight: "900",
+              }}
+            >
+              Evstart
+            </Link>
+            <button
+              className="navbar-toggler bg-white"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                {showNav.map((item) => {
+                  return (
+                    <li className="nav-item" key={item.id}>
+                      <Link
+                        className="nav-link active"
+                        aria-current="page"
+                        style={{
+                          fontWeight: "bold",
+                        }}
+                        to={item.path}
+                      >
+                        {item.name}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
-      <div style={{
-        minHeight: "100vh"
-      }}>
+
+     
+      <div
+        style={{
+          minHeight: "100vh",
+        }}
+      >
         <Outlet />
       </div>
       <div>
         <Footer />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
